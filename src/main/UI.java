4d5ffb4ca9a66372e124854d188e0cf6a9a93859
adrianfhr <adrianfahriaffandi@gmail.com>
2023-05-06 20:19:07 +0700

    update

--- src/main/UI.java
+++ src/main/UI.java
@@ -193,7 +193,8 @@ public class UI {
                 gamePanel.isInputAction = true;
                 gamePanel.playMusic(0);
                 String input = JOptionPane.showInputDialog(null, "Masukkan nama Sim:");
-                if(input == null || input == "") input = "Player 1";
+                System.out.println(input);
+                if(input == null || input.equals("")) input = "Player 1";
                 gamePanel.player[gamePanel.currentPlayer].setName(input);
                 gamePanel.isInputAction = false;
                 break;
